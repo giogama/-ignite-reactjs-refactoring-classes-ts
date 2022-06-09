6f30f2f7c53f6a431c4b7e741e6af097275f332f
giogama <giogama@hotmail.com>
2022-06-09 09:56:14 -0300

    All project converted from javascript to typescript

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -31,31 +31,4 @@ const Header = ({openModal}:HeaderProps): JSX.Element => {
   )
 }
 
-// class Header extends Component {
-//   render() {
-//     const { openModal } = this.props;
-
-//     return (
-//       <Container>
-//         <header>
-//           <img src={Logo} alt="GoRestaurant" />
-//           <nav>
-//             <div>
-//               <button
-//                 type="button"
-//                 onClick={openModal}
-//               >
-//                 <div className="text">Novo Prato</div>
-//                 <div className="icon">
-//                   <FiPlusSquare size={24} />
-//                 </div>
-//               </button>
-//             </div>
-//           </nav>
-//         </header>
-//       </Container>
-//     )
-//   }
-// };
-
 export default Header;
